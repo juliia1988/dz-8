982cab81b1a59645a6f00ba6ee2004a3323a626e
Yuliia Sokolova <65017585+juliia1988@users.noreply.github.com>
2023-04-12 17:44:07 +0200

    fix

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 5; i > 0; i--) {
             int factorial = 1;
-            factorial = 1;
             for (int j = 1; j <= i; j++) {
                 factorial = factorial * j;
             }
